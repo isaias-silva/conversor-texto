7054ce435112b8e5a90dcdd4104d9d93b6412a9a
isaias-silva <zackblackmangold@gmail.com>
2021-06-27 23:25:20 -0300

    codigos

---
+++ script.js
@@ -0,0 +1,15 @@
+function mudar(v) {
+    let textarea = document.querySelector("#txt")
+    if (textarea.value != "") {
+        if (v == "maiu") {
+            textarea.value = (textarea.value).toUpperCase()
+        } else if (v == "minu") {
+            textarea.value = (textarea.value).toLowerCase()
+        } else if (v == "maiu_ini") {
+            if (textarea.value[0] != " ") {
+                textarea.value = (textarea.value)[0].toUpperCase() + (textarea.value).slice(1).toLowerCase()
+
+            }
+        }
+    }
+}
